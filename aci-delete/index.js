eb8b88b55327148a33b67d8026a115ec366e7585
magpieproperties <42524825+magpieproperties@users.noreply.github.com>
2018-08-30 21:37:00 -0400

    Add files via upload

--- aci-delete/index.js
+++ aci-delete/index.js
@@ -1,1 +1,30 @@
-Test
+'use strict'
+
+var msRestAzure = require("ms-rest-azure");
+//var resourceManagement = require('azure-arm-resource');
+var containerInstance = require('azure-arm-containerinstance');
+
+var clientId = process.env.client_id,
+    secret = process.env.client_secret,
+    domain = process.env.tenant,
+    subscriptionId = process.env.subscription_id,
+    resourceGroupName = process.env.resourceGroup;
+
+module.exports = function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+    
+    msRestAzure.loginWithServicePrincipalSecret(clientId, secret, domain, function(err, credentials) {
+        let client = new containerInstance.ContainerInstanceManagementClient(credentials,subscriptionId);
+        let containerGroupName = "gnarlytaplist";
+
+        client.containerGroups.deleteMethod(resourceGroupName, containerGroupName)
+            .then( (cgroup) => {
+                context.log(cgroup)
+            }).catch((err) => {
+                context.log(err);
+                return;
+            });
+    });
+
+    context.done();
+};
